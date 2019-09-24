fbd9e8ec8deadd320ea63f40cd0c4b1afa64f1fa
Mingjing Tang <tmjcsh@gmail.com>
2019-09-24 12:25:23 -0400

    loadMore success

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -1,20 +1,24 @@
+document.querySelector("body").querySelector("#loadMore").hidden = true;
 document.querySelector("form").addEventListener("submit", getGif);
 let gallery = document.querySelector(".gif-gallery");
 
-function getGif(event){
-    event.preventDefault();
+function getGif(event, offset=0){
+    if(event){
+        event.preventDefault();
+
+        let child = gallery.lastElementChild;
+        while(child){
+            gallery.removeChild(child);
+            child = gallery.lastElementChild;
+        }
+    }
+    // event && event.preventDefault();
 
     let apiKey = "1D6TfNiI9D9OHCqa7SCm2BttZwg6kdBY";
     let category = document.querySelector(".form-control").value;
+    console.log("offset is: " +offset)
 
-
-    let child = gallery.lastElementChild;
-    while(child){
-        gallery.removeChild(child);
-        child = gallery.lastElementChild;
-    }
-
-    fetch(`http://api.giphy.com/v1/gifs/search?q=${category}&api_key=${apiKey}`,{
+    fetch(`http://api.giphy.com/v1/gifs/search?q=${category}&api_key=${apiKey}&offset=${offset}`,{
         method: "GET", 
     })
     .then((response)=>
@@ -26,7 +30,7 @@ function getGif(event){
 
 function onSuccess(json){
     for(let i = 0; i < 10; i++){
-        console.log(json.data);
+        // console.log(json.data);
         let image = json.data[i].embed_url;
         let showImage = document.createElement("iframe");
 
@@ -37,35 +41,36 @@ function onSuccess(json){
         gallery.appendChild(showImage);
     }
 
-    //set a new button
-    let button = document.createElement("BUTTON");
-    button.setAttribute("id","moreButton");
-    let buttonText = document.createTextNode("load more");
-    button.appendChild(buttonText)
-    
-    //get more image
-    gallery.appendChild(button);
-    gallery.querySelector("#moreButton").addEventListener("click",function() {
+    if(document.querySelector("body").querySelector("#loadMore").hidden === true){
+        document.querySelector("body").querySelector("#loadMore").hidden = false;
+
+    document.querySelector("body").querySelector("#loadMore").addEventListener("click",function() {
         moreImage(json);
-      });
+    });
+    }
 }
+
+
 
 function onError(error) {
     alert("Sorry, there was a problem!");
     console.dir(error);
 }
+
 function moreImage(jsonData){
-    console.log(jsonData.data);
-    for(let i = 10; i < 20; i++){
-        let image = jsonData.data[i].embed_url;
-        let showImage = document.createElement("iframe");
+    // console.log(jsonData.data);
+    // for(let i = 10; i < 20; i++){
+    //     let image = jsonData.data[i].embed_url;
+    //     let showImage = document.createElement("iframe");
 
-        showImage.setAttribute('width','100em');
-        showImage.setAttribute('height','100em');
-        showImage.setAttribute('src',image);
+    //     showImage.setAttribute('width','100em');
+    //     showImage.setAttribute('height','100em');
+    //     showImage.setAttribute('src',image);
 
-        gallery.appendChild(showImage);
-    }
+    //     gallery.appendChild(showImage);
+    // }
+    let offsetCount =  document.querySelector(".gif-gallery").childElementCount;
+    getGif(null, offsetCount);
 }
 
 
